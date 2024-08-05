299230efbc8dc1355acaa53efbdd4b0499483fc9
NoTimeForHero <notimeforhero@yandex.ru>
2024-08-05 15:39:58 +0300

    fix: added default export for YandexMetricaMultiProvider

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { YandexMetricaProvider } from './components/YandexMetricaProvider';
+export { YandexMetricaMultiProvider, YandexMetricaProvider } from './components/YandexMetricaProvider';
 export { useMetrica } from './hooks/useMetrica';
 export { ym } from './lib/ym';
